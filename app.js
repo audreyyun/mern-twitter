1755220331ef35642a0b4c5b8339a6dce39ed7b2
audreyyun <96audreyyun@gmail.com>
2021-02-01 09:30:53 -0500

    frontend

--- app.js
+++ app.js
@@ -1,9 +1,38 @@
 const express = require("express");
 const app = express(); //app object we want to configure
+const mongoose = require("mongoose");
+const db = require("./config/keys").mongoURI; 
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const User = require('./models/User');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+
+mongoose
+    .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("Connected to mongoDB"))
+    .catch(err => console.log(err));
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-    res.send("Hello World!")
+    const user = new User({
+        handle: "jim",
+        email: "jim@jim.jim",
+        password: "jimisgreat123"
+    })
+    user.save();
 });
+
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
 
 const port = process.env.PORT || 5000;
 
